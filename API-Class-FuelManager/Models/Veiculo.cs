9f5bd400d2a585562240247a55f0bbc93f7cfe51
Bianca Dellevedove Matsumura <bdmatsumura@sga.pucminas.br>
2026-05-28 22:27:54 -0300

    Adicionar arquivos de projeto.

---
+++ API-Class-FuelManager/Models/Veiculo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Class_FuelManager.Models
+{
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Modelo { get; set; }
+        [Required]
+        public int AnoFabricacao { get; set; }
+        [Required]
+        public int AnoModelo { get; set; }
+
+    }
+}
