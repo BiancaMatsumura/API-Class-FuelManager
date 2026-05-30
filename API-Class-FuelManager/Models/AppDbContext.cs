d3a63e8556db8897b6dd26aac52a45703e9cf205
Bianca Dellevedove Matsumura <bdmatsumura@sga.pucminas.br>
2026-05-30 18:14:43 -0300

    Configuração do EntityFramework

---
+++ API-Class-FuelManager/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_Class_FuelManager.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Consumo> Consumos { get; set; }
+    }
+}
